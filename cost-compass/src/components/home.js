de36a077ca21f0979b59c7dc721d0f8a7ab3ebc0
lynettehemingway <147886775+lynettehemingway@users.noreply.github.com>
2024-09-28 10:00:19 -0400

    updated home.js

--- cost-compass/src/components/home.js
+++ cost-compass/src/components/home.js
@@ -2,12 +2,14 @@ import Navbar from "./Navbar/navbar";
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
 //import Intro from "./components/Intro/intro";
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 function Home(){
     return (
         <>
         <Navbar />
         
-        <APIProvider apiKey={'api-key'} onLoad={() => console.log('Maps API has loaded.')}>
+        <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
             <Map
             defaultZoom={13}
             defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
