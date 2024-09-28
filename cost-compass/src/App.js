ca92056e1c33e9cb01d8f9b14878cbca5fa0ec41
CloudRazerz <cloudrazerz@gmail.com>
2024-09-28 09:17:38 -0400

    added dotenv

--- cost-compass/src/App.js
+++ cost-compass/src/App.js
@@ -23,6 +23,8 @@ import {APIProvider, Map} from '@vis.gl/react-google-maps'
 //   );
 // };
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 function App() {
 
 
@@ -31,7 +33,7 @@ function App() {
       <Navbar />
       {/* <Intro />
       <Resources /> */}
-      <APIProvider apiKey={'api-key'} onLoad={() => console.log('Maps API has loaded.')}>
+      <APIProvider apiKey={apiKey} onLoad={() => console.log('Maps API has loaded.')}>
     <Map
       defaultZoom={13}
       defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
