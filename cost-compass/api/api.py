76be2e1767362194b1749d3e2d6b3f8987b1ff1e
Tanushree Hadavale <147886719+thadavale@users.noreply.github.com>
2024-09-28 21:06:20 -0400

    Update api.py

--- cost-compass/api/api.py
+++ cost-compass/api/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from countyInfo import get_location, get_county_data
 
 app = Flask(__name__)
 
@@ -7,4 +8,9 @@ def reverse_geocode():
     longitude = request.json['longitude']
     latitude = request.json['latitude']
     print(longitude, latitude)
+
+    [county, state] = get_location(latitude, longitude)
+
+    stats = get_county_data(county, state)
+
     return {'latitude': latitude, 'longitude': longitude}
