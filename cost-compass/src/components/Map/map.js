e039d5c0fee6a4b9ed37fba3930aea3527101b40
CloudRazerz <cloudrazerz@gmail.com>
2024-09-29 05:52:13 -0400

    change font

--- cost-compass/src/components/Map/map.js
+++ cost-compass/src/components/Map/map.js
@@ -28,12 +28,12 @@ function MyMapComponent() {
                         <div className="infotab">
                             <h2 className="kode-mono-o"><span className="color kode-mono-o">{data.location_data.county}</span>, {data.location_data.state}</h2>
                             <p className="kode-mono-o">{data.description}</p>
-                            <p className="kode-mono-o">Population: {data.county_data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            <p className="kode-mono-o">Median Home Value: ${data.county_data.home_value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            <p className="kode-mono-o">Median Housing Cost: ${data.county_data.housing_cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            <p className="kode-mono-o">Median Household Income: ${data.county_data.household_income.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            <p className="kode-mono-o">Unemployment Rate: {data.county_data.unemployment_rate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}%</p>
-                            <p className="kode-mono-o">Poverty Rate: {data.county_data.poverty_rate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}%</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Population:</span> {data.county_data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Median Home Value:</span> ${data.county_data.home_value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Median Housing Cost:</span> ${data.county_data.housing_cost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Median Household Income:</span> ${data.county_data.household_income.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Unemployment Rate:</span> {data.county_data.unemployment_rate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}%</p>
+                            <p className="sans-serif"><span className="color kode-mono-o">Poverty Rate:</span> {data.county_data.poverty_rate.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}%</p>
                         </div>
                     )
                 }
