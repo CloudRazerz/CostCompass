00d422a9d099495c458eb60a1d46ba6e7429e067
CloudRazerz <cloudrazerz@gmail.com>
2024-09-28 21:20:31 -0400

    changed map bounds

--- cost-compass/src/components/Map/map.js
+++ cost-compass/src/components/Map/map.js
@@ -29,10 +29,21 @@ function MyMapComponent() {
         <Map
             defaultZoom={6.75}
             defaultCenter={{ lat: 28.1, lng: -82.5 }}
+            minZoom={6.75}
+            maxZoom={6.75}
+            options={{
+                restriction: {
+                  latLngBounds: {east: -65, north: 51, south: 24, west:-126},
+                  strictBounds: false,
+                },
+                
+                clickableIcons: false,
+                disableDefaultUI: true,
+                keyboardShortcuts: false
+            }}
+            
             onClick={handleMapClick}
-            gestureHandling={'none'}
-            disableDefaultUI={'true'}
-            keyboardShortcuts={'false'}
+
             >
             {marker}
         </Map>
